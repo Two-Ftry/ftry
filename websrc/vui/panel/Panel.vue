aea3c983bc07184d64ebb77c9e680a8ed178bb34
suifengfengye <1527261080@qq.com>
2016-09-19 22:14:58 +0800

    sub

--- websrc/vui/panel/Panel.vue
+++ websrc/vui/panel/Panel.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="panel-box">
+  <div class="panel-box"  transition="moonlight">
    <div class="panel-mask" @click="onToClose"></div>
     面板内容
   </div>
@@ -38,7 +38,47 @@ export default {
     right: 0;
     height: 100%;
     z-index: 99;
-    background-color: rgba(255, 255, 255, 0.1);
+    background-color: rgba(255, 255, 255, 0.1); /* IE */
     /*background-color: transparent;*/
   }
+  .moonlight-enter {
+        -webkit-transform: translateX(110%);
+        transform: translateX(110%);
+        /*-mz-animation: moonlight-in .4s ease-out forwards;
+        -webkit-animation: moonlight-in .4s ease-out forwards;*/
+        animation: moonlight-in .4s ease-out forwards;
+    }
+
+    .moonlight-leave {
+        /*-mz-animation: moonlight-out .4s ease-in forwards;
+        -webkit-animation: moonlight-out .4s ease-in forwards;*/
+        animation: moonlight-out .4s ease-in forwards;
+    }
+
+    @keyframes moonlight-in {
+        0% {
+            transform: translateX(110%);
+        }
+
+        100% {
+            transform: translateX(0%);
+        }
+    }
+    @-webkit-keyframes moonlight-out {
+        0% {
+            transform: translateX(0%);
+        }
+        100% {
+            transform: translateX(110%);
+        }
+    }
+    @keyframes moonlight-out {
+        0% {
+            transform: translateX(0%);
+        }
+        100% {
+            transform: translateX(110%);
+        }
+    }
+
 </style>
